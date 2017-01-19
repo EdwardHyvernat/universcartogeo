632f4908dbd57cc423d32ed636734580e6a526b4
EdwardHyvernat <edouard.hyvernat@gmail.com>
2017-01-19 16:37:18 +0100

    pleins de modifs sur Leaflet blog

--- js/leaflet_blog.js
+++ js/leaflet_blog.js
@@ -22,10 +22,12 @@
 
 // Simplification
 
+// SIMPLE CARTE WMS IGN
+/*
 function go() {
     var map = L.map('mapblog', {
         crs: L.geoportalCRS.EPSG2154,
-    }).setView([48.9, 2.5], 12);
+    }).setView([48.9, 2.5], 17);
     var lyr = L.geoportalLayer.WMTS({
         layer: "CADASTRALPARCELS.PARCELS.L93"
     });
@@ -36,3 +38,45 @@ Gp.Services.getConfig({
     apiKey: "9cxflgtlfpitopej4q3x9wby",
     onSuccess: go
 });
+
+var infoDiv = document.getElementById("commentairescarto");
+infoDiv.innerHTML = "<p> Fond de plan cadastre IGN sur client Leaflet JS (" + Gp.leafletExtDate + ")</p>";
+*/
+
+function go() {
+    map = L.map("mapblog").setView([48.9, 2.5], 13);
+    var lyrOSM= L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?') ;
+    var lyrOrtho = L.geoportalLayer.WMTS({
+        layer: "ORTHOIMAGERY.ORTHOPHOTOS",
+    });
+    var lyrMaps = L.geoportalLayer.WMTS({
+        layer: "GEOGRAPHICALGRIDSYSTEMS.MAPS",
+    }, { // leafletParams
+        opacity: 0.7
+    });
+
+    map.addLayer(lyrOrtho);
+    map.addLayer(lyrOSM) ;
+    map.addLayer(lyrMaps) ;
+	var searchCtrl = L.geoportalControl.SearchEngine({});
+		map.addControl(searchCtrl);
+    var layerSwitcher = L.geoportalControl.LayerSwitcher({
+    	layers : [{
+        	layer : lyrOSM,
+            config : {
+            	title : "OSM",
+                description : "Couche Open Street Maps"
+            }
+        }]
+    });
+	map.addControl(layerSwitcher);
+	
+}
+
+Gp.Services.getConfig({
+    apiKey : "9cxflgtlfpitopej4q3x9wby",
+    onSuccess : go
+}) ;
+
+var infoDiv= document.getElementById("commentairescarto") ;
+infoDiv.innerHTML= "<p> Carte multi-couches WMS IGN  ("+Gp.leafletExtDate+")</p>" ;
