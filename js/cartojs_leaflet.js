7a64772a7ed2861e3a3b6c69a2d61a4f7eb4f994
EdwardHyvernat <edouard.hyvernat@gmail.com>
2017-06-29 14:57:55 +0200

    Markers with custom icons & color

--- js/cartojs_leaflet.js
+++ js/cartojs_leaflet.js
@@ -1,4 +1,4 @@
-var mbUrl = 'http://api.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZWR3YXJkaCIsImEiOiJjaXpxd3h3MDkwMDE3MzJxdTF1enljZ2ViIn0.ewaeb0hRCWBOrQZv4kfuvw'
+mbUrl = 'http://api.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZWR3YXJkaCIsImEiOiJjaXpxd3h3MDkwMDE3MzJxdTF1enljZ2ViIn0.ewaeb0hRCWBOrQZv4kfuvw'
 
 var gris     		  = L.tileLayer(mbUrl, {id: 'mapbox.light'}),
 	streets  		  = L.tileLayer(mbUrl, {id: 'mapbox.streets'}),
@@ -9,19 +9,47 @@ var gris     		  = L.tileLayer(mbUrl, {id: 'mapbox.light'}),
 	satellite		  = L.tileLayer(mbUrl, {id:	'mapbox.satellite'}),
 	sport             = L.tileLayer(mbUrl, {id: 'mapbox.run-bike-hike'});
 
+var vertIcon = L.icon({
+    iconUrl: "../img/leaflet/marqueur_vert.png",
+    iconSize:     [25, 41],
+	iconAnchor: [12, 41],
+    popupAnchor: [0, -41]
+});	
+
+var bleuIcon = L.icon({
+    iconUrl: "../img/leaflet/marqueur_bleu.png",
+    iconSize:     [25, 41],
+	iconAnchor: [12, 41],
+    popupAnchor: [0, -41]
+});
+
+var rougeIcon = L.icon({
+    iconUrl: "../img/leaflet/marqueur_rouge.png",
+    iconSize:     [25, 41],
+	iconAnchor: [12, 41],
+    popupAnchor: [0, -41]
+});
+
+var violetIcon = L.icon({
+    iconUrl: "../img/leaflet/marqueur_violet.png",
+    iconSize:     [25, 41],
+	iconAnchor: [12, 41],
+    popupAnchor: [0, -41]
+});
+	
 var rouge = new L.LayerGroup();
 var violet = new L.LayerGroup();
 var vert = new L.LayerGroup();
 var bleu = new L.LayerGroup();
 
-L.marker([49.04211, 1.57195]).bindPopup('Résidence').addTo(rouge),
-L.marker([48.83102, 2.34052]).bindPopup('Travail').addTo(violet),
-L.marker([48.8439, 2.42658]).bindPopup('Vie privée').addTo(vert),
-L.marker([49.6498, 0.92926]).bindPopup('Vie privée').addTo(vert),
-L.marker([48.84466, 2.34258]).bindPopup('Etudes').addTo(bleu);
+L.marker([49.04211, 1.57195] ,{icon: rougeIcon}).bindPopup('Résidence').addTo(rouge),
+L.marker([48.83102, 2.34052] ,{icon: violetIcon}).bindPopup('Travail').addTo(violet),
+L.marker([48.8439, 2.42658]  ,{icon: vertIcon}).bindPopup('Vie privée').addTo(vert),
+L.marker([49.6498, 0.92926]  ,{icon: vertIcon}).bindPopup('Vie privée').addTo(vert),
+L.marker([48.84466, 2.34258] ,{icon: bleuIcon}).bindPopup('Etudes').addTo(bleu);
 
 		
-var carto = L.map('mapleaflet', {center: [49, 1], zoom: 7, layers: [gris, rouge, violet, vert, bleu]});
+var carto = L.map('mapleaflet', {center: [49, 1], zoom: 7, layers: [streets, rouge, violet, vert, bleu]});
 
 
 
